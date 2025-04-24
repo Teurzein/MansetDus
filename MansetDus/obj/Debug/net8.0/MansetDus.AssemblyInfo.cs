cb8caabd4f9bfae8976ae8d49424e63688590a26
Teurzein <59874312+Teurzein@users.noreply.github.com>
2025-04-24 17:45:02 +0300

    fasfs

--- MansetDus/obj/Debug/net8.0/MansetDus.AssemblyInfo.cs
+++ MansetDus/obj/Debug/net8.0/MansetDus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MansetDus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6717c54d9069954c416ef43fdfd93e89808a4280")]
 [assembly: System.Reflection.AssemblyProductAttribute("MansetDus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MansetDus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
